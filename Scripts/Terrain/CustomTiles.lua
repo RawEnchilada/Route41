be7a9cd8a469567c83fbe4963d95e7ccf9b40e87
RawEnchilada <wittmann.aron@gmail.com>
2024-05-10 00:28:53 +0200

    extra poi

--- Scripts/Terrain/CustomTiles.lua
+++ Scripts/Terrain/CustomTiles.lua
@@ -1,231 +1,232 @@
 local g_desert = {} --Flags lookup table
 
-local function toDesertIndex( se, sw, nw, ne )
-	return bit.bor( bit.lshift( se, 3 ), bit.lshift( sw, 2 ), bit.lshift( nw, 1 ), bit.tobit( ne ) )
+local function toDesertIndex(se, sw, nw, ne)
+    return bit.bor(bit.lshift(se, 3), bit.lshift(sw, 2), bit.lshift(nw, 1), bit.tobit(ne))
 end
 
 function initCustomTiles()
+    -- Desert tiles
+    g_desert = {
+        AddTile(3001501, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_01.tile", 5),
+        AddTile(3001502, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_02.tile", 5),
+        AddTile(3001503, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_03.tile", 5),
+        AddTile(3001504, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_04.tile", 5),
+        AddTile(3001505, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_05.tile", 5),
+        AddTile(3001506, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_06.tile", 5),
+        AddTile(3001507, "$CONTENT_DATA/Terrain/Tiles/desert_01.tile", 5),
+        AddTile(3001508, "$CONTENT_DATA/Terrain/Tiles/desert_02.tile", 5),
+        AddTile(3001509, "$CONTENT_DATA/Terrain/Tiles/desert_03.tile", 5),
+        AddTile(3001510, "$CONTENT_DATA/Terrain/Tiles/desert_04.tile", 5),
+        AddTile(3001511, "$CONTENT_DATA/Terrain/Tiles/desert_05.tile", 5),
+        AddTile(3001512, "$CONTENT_DATA/Terrain/Tiles/desert_06.tile", 5),
+        AddTile(3001513, "$CONTENT_DATA/Terrain/Tiles/desert_farmbot_01.tile", 1),
+    }
 
-  -- Desert tiles
-	g_desert = {
-		AddTile( 3001501, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_01.tile", 5 ), 
-		AddTile( 3001502, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_02.tile", 5 ), 
-		AddTile( 3001503, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_03.tile", 5 ), 
-		AddTile( 3001504, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_04.tile", 5 ), 
-		AddTile( 3001505, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_05.tile", 5 ), 
-		AddTile( 3001506, "$SURVIVAL_DATA/Terrain/Tiles/desert/Desert(1111)_06.tile", 5 ),
-		AddTile( 3001507, "$CONTENT_DATA/Terrain/Tiles/desert_01.tile", 5 ),
-		AddTile( 3001508, "$CONTENT_DATA/Terrain/Tiles/desert_02.tile", 5 ),
-		AddTile( 3001509, "$CONTENT_DATA/Terrain/Tiles/desert_03.tile", 5 ),
-		AddTile( 3001510, "$CONTENT_DATA/Terrain/Tiles/desert_04.tile", 5 ),
-		AddTile( 3001511, "$CONTENT_DATA/Terrain/Tiles/desert_05.tile", 5 ),
-        AddTile( 3001512, "$CONTENT_DATA/Terrain/Tiles/desert_06.tile", 5 ),
-        AddTile( 3001513, "$CONTENT_DATA/Terrain/Tiles/desert_farmbot_01.tile", 1 ),
-	}
+    -- Just north/south straight road tiles with no cliff data
+    g_roads = {
+        tiles = {
+            AddTile(1128001, "$CONTENT_DATA/Terrain/Tiles/road_01.tile"),
+            AddTile(1128002, "$CONTENT_DATA/Terrain/Tiles/road_02.tile"),
+            AddTile(1128003, "$CONTENT_DATA/Terrain/Tiles/road_03.tile"),
+            AddTile(1128004, "$CONTENT_DATA/Terrain/Tiles/road_04.tile"),
+            AddTile(1128005, "$CONTENT_DATA/Terrain/Tiles/road_05.tile"),
+            AddTile(1128006, "$CONTENT_DATA/Terrain/Tiles/road_06.tile"),
+            AddTile(1128007, "$CONTENT_DATA/Terrain/Tiles/road_07.tile"),
+            AddTile(1128008, "$CONTENT_DATA/Terrain/Tiles/road_08.tile"),
+            AddTile(1128009, "$CONTENT_DATA/Terrain/Tiles/road_09.tile"),
+            AddTile(1128010, "$CONTENT_DATA/Terrain/Tiles/road_10.tile"),
+            AddTile(1128011, "$CONTENT_DATA/Terrain/Tiles/road_11.tile"),
+            AddTile(1128012, "$CONTENT_DATA/Terrain/Tiles/road_12.tile"),
+            AddTile(1128013, "$CONTENT_DATA/Terrain/Tiles/road_13.tile"),
+            AddTile(1128014, "$CONTENT_DATA/Terrain/Tiles/road_14.tile"),
+            AddTile(1128015, "$CONTENT_DATA/Terrain/Tiles/road_15.tile"),
+            AddTile(1128016, "$CONTENT_DATA/Terrain/Tiles/road_16.tile"),
+            AddTile(1128017, "$CONTENT_DATA/Terrain/Tiles/road_17.tile"),
+            AddTile(1128018, "$CONTENT_DATA/Terrain/Tiles/road_18.tile"),
+            AddTile(1128019, "$CONTENT_DATA/Terrain/Tiles/road_19.tile"),
+            AddTile(1128020, "$CONTENT_DATA/Terrain/Tiles/road_20.tile"),
+            AddTile(1128021, "$CONTENT_DATA/Terrain/Tiles/road_21.tile"),
+        },
+        rotation = 3
+    }
 
-  -- Just north/south straight road tiles with no cliff data
-  g_roads = { 
-		tiles = { 
-			AddTile( 1128001, "$CONTENT_DATA/Terrain/Tiles/road_01.tile" ), 
-			AddTile( 1128002, "$CONTENT_DATA/Terrain/Tiles/road_02.tile" ), 
-			AddTile( 1128003, "$CONTENT_DATA/Terrain/Tiles/road_03.tile" ), 
-			AddTile( 1128004, "$CONTENT_DATA/Terrain/Tiles/road_04.tile" ), 
-			AddTile( 1128005, "$CONTENT_DATA/Terrain/Tiles/road_05.tile" ), 
-			AddTile( 1128006, "$CONTENT_DATA/Terrain/Tiles/road_06.tile" ), 
-			AddTile( 1128007, "$CONTENT_DATA/Terrain/Tiles/road_07.tile" ), 
-			AddTile( 1128008, "$CONTENT_DATA/Terrain/Tiles/road_08.tile" ), 
-			AddTile( 1128009, "$CONTENT_DATA/Terrain/Tiles/road_09.tile" ), 
-			AddTile( 1128010, "$CONTENT_DATA/Terrain/Tiles/road_10.tile" ), 
-			AddTile( 1128011, "$CONTENT_DATA/Terrain/Tiles/road_11.tile" ), 
-			AddTile( 1128012, "$CONTENT_DATA/Terrain/Tiles/road_12.tile" ), 
-			AddTile( 1128013, "$CONTENT_DATA/Terrain/Tiles/road_13.tile" ), 
-			AddTile( 1128014, "$CONTENT_DATA/Terrain/Tiles/road_14.tile" ), 
-			AddTile( 1128015, "$CONTENT_DATA/Terrain/Tiles/road_15.tile" ), 
-			AddTile( 1128016, "$CONTENT_DATA/Terrain/Tiles/road_16.tile" ),
-			AddTile( 1128017, "$CONTENT_DATA/Terrain/Tiles/road_17.tile" ),
-			AddTile( 1128018, "$CONTENT_DATA/Terrain/Tiles/road_18.tile" ),
-			AddTile( 1128019, "$CONTENT_DATA/Terrain/Tiles/road_19.tile" ),
-			AddTile( 1128020, "$CONTENT_DATA/Terrain/Tiles/road_20.tile" ),
-			AddTile( 1128021, "$CONTENT_DATA/Terrain/Tiles/road_21.tile" ),
-		},
-		rotation = 3
-	}
+    g_road_ends = {
+        AddTile(1293000, "$CONTENT_DATA/Terrain/Tiles/road_end.tile"),
+    }
 
-  g_road_ends = { 
-		AddTile( 1293000, "$CONTENT_DATA/Terrain/Tiles/road_end.tile" ), 
-	}
-	
-  g_starter_connectors = { 
-		AddTile( 5323001, "$CONTENT_DATA/Terrain/Tiles/road_start.tile" ), 
-	}
+    g_starter_connectors = {
+        AddTile(5323001, "$CONTENT_DATA/Terrain/Tiles/road_start.tile"),
+    }
 
-  g_elevators = { 
-		AddTile( 9423000, "$CONTENT_DATA/Terrain/Tiles/elevator.tile" ), 
-	}
+    g_elevators = {
+        AddTile(9423000, "$CONTENT_DATA/Terrain/Tiles/elevator.tile"),
+    }
 
-  g_fences = {
-		AddTile( 5002500, "$CONTENT_DATA/Terrain/Tiles/fence_01.tile", 5 ),
-		AddTile( 5002501, "$CONTENT_DATA/Terrain/Tiles/fence_02.tile", 5 ),
-	}
+    g_fences = {
+        AddTile(5002500, "$CONTENT_DATA/Terrain/Tiles/fence_01.tile", 5),
+        AddTile(5002501, "$CONTENT_DATA/Terrain/Tiles/fence_02.tile", 5),
+    }
 
-  g_fence_corners = {
-		AddTile( 5002600, "$CONTENT_DATA/Terrain/Tiles/fence_corner_01.tile", 5 ),
-	}
+    g_fence_corners = {
+        AddTile(5002600, "$CONTENT_DATA/Terrain/Tiles/fence_corner_01.tile", 5),
+    }
 
-  g_scorched = {
-		AddTile( 1232500, "$CONTENT_DATA/Terrain/Tiles/scorched_01.tile", 5 ),
-		AddTile( 1232501, "$CONTENT_DATA/Terrain/Tiles/scorched_02.tile", 5 ),
-		AddTile( 1232502, "$CONTENT_DATA/Terrain/Tiles/scorched_03.tile", 5 ),
-		AddTile( 1232503, "$CONTENT_DATA/Terrain/Tiles/scorched_04.tile", 5 ),
-	}
+    g_scorched = {
+        AddTile(1232500, "$CONTENT_DATA/Terrain/Tiles/scorched_01.tile", 5),
+        AddTile(1232501, "$CONTENT_DATA/Terrain/Tiles/scorched_02.tile", 5),
+        AddTile(1232502, "$CONTENT_DATA/Terrain/Tiles/scorched_03.tile", 5),
+        AddTile(1232503, "$CONTENT_DATA/Terrain/Tiles/scorched_04.tile", 5),
+    }
 
-	g_starter_houses = {
-		AddTile( 1222500, "$CONTENT_DATA/Terrain/Tiles/starter_house.tile" )
-	}
-	
-	g_desert_pois = {
-		{tile = AddTile( 3210300, "$CONTENT_DATA/Terrain/Tiles/poi_01.tile", 4 ), size = 4},
-		{tile = AddTile( 3210301, "$CONTENT_DATA/Terrain/Tiles/poi_02.tile", 4 ), size = 2},
-		{tile = AddTile( 3210302, "$CONTENT_DATA/Terrain/Tiles/poi_ship_01.tile", 1 ), size = 1},
-		{tile = AddTile( 3210303, "$CONTENT_DATA/Terrain/Tiles/poi_tower_01.tile", 4 ), size = 1},
-        {tile = AddTile( 3210304, "$CONTENT_DATA/Terrain/Tiles/poi_bunker_01.tile", 2 ), size = 1},
-        {tile = AddTile( 3210305, "$CONTENT_DATA/Terrain/Tiles/poi_cloth_shop.tile", 1 ), size = 1},
-        {tile = AddTile( 3210306, "$CONTENT_DATA/Terrain/Tiles/poi_shack_01.tile", 3 ), size = 1},
-        {tile = AddTile( 3210307, "$CONTENT_DATA/Terrain/Tiles/poi_pasture_01.tile", 2 ), size = 1},
-        {tile = AddTile( 3210308, "$CONTENT_DATA/Terrain/Tiles/poi_small_loot_01.tile", 2 ), size = 1},
-	}
+    g_starter_houses = {
+        AddTile(1222500, "$CONTENT_DATA/Terrain/Tiles/starter_house.tile")
+    }
 
-	-- Commented tiles are desert-ified kiosk tiles from survival
-	-- Flippable lets the tile be on the other side of the road, rotates by 180 as well
-	g_road_pois = {
-	-- {tile = AddTile( 4201001, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-	-- {tile = AddTile( 4201002, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_02.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-	-- {tile = AddTile( 4201003, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_03.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-		{tile = AddTile( 4201004, "$CONTENT_DATA/Terrain/Tiles/rpoi_shop_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-		{tile = AddTile( 4201005, "$CONTENT_DATA/Terrain/Tiles/rpoi_petrol_station_01.tile", 5 ), size = 1, offset = 1, rotation = 0, flippable = true},
-		{tile = AddTile( 4201006, "$CONTENT_DATA/Terrain/Tiles/rpoi_garage_01.tile", 5 ), size = 1, offset = 0, rotation = 1, flippable = true},
-		{tile = AddTile( 4201007, "$CONTENT_DATA/Terrain/Tiles/rpoi_observation_tower_01.tile", 5 ), size = 1, offset = 2, rotation = 0, flippable = true},
-		{tile = AddTile( 4201008, "$CONTENT_DATA/Terrain/Tiles/rpoi_bus_stop_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-		{tile = AddTile( 4201009, "$CONTENT_DATA/Terrain/Tiles/rpoi_radio_station_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-		{tile = AddTile( 42010010, "$CONTENT_DATA/Terrain/Tiles/rpoi_food_truck_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-		{tile = AddTile( 42010011, "$CONTENT_DATA/Terrain/Tiles/rpoi_car_wreck_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
-	}
+    g_desert_pois = {
+        { tile = AddTile(3210300, "$CONTENT_DATA/Terrain/Tiles/poi_01.tile", 4),              size = 4 },
+        { tile = AddTile(3210301, "$CONTENT_DATA/Terrain/Tiles/poi_02.tile", 4),              size = 2 },
+        { tile = AddTile(3210302, "$CONTENT_DATA/Terrain/Tiles/poi_ship_01.tile", 1),         size = 1 },
+        { tile = AddTile(3210303, "$CONTENT_DATA/Terrain/Tiles/poi_tower_01.tile", 4),        size = 1 },
+        { tile = AddTile(3210304, "$CONTENT_DATA/Terrain/Tiles/poi_bunker_01.tile", 2),       size = 1 },
+        { tile = AddTile(3210305, "$CONTENT_DATA/Terrain/Tiles/poi_cloth_shop.tile", 1),      size = 1 },
+        { tile = AddTile(3210306, "$CONTENT_DATA/Terrain/Tiles/poi_shack_01.tile", 3),        size = 1 },
+        { tile = AddTile(3210307, "$CONTENT_DATA/Terrain/Tiles/poi_pasture_01.tile", 2),      size = 1 },
+        { tile = AddTile(3210308, "$CONTENT_DATA/Terrain/Tiles/poi_small_loot_01.tile", 2),   size = 1 },
+        { tile = AddTile(3210309, "$CONTENT_DATA/Terrain/Tiles/poi_bike_wreck_01.tile", 2),   size = 1 },
+    }
+
+    -- Commented tiles are desert-ified kiosk tiles from survival
+    -- Flippable lets the tile be on the other side of the road, rotates by 180 as well
+    g_road_pois = {
+        -- {tile = AddTile( 4201001, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_01.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
+        -- {tile = AddTile( 4201002, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_02.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
+        -- {tile = AddTile( 4201003, "$CONTENT_DATA/Terrain/Tiles/rpoi_kiosk_64_03.tile", 5 ), size = 1, offset = 0, rotation = 3, flippable = true},
+        { tile = AddTile(4201004, "$CONTENT_DATA/Terrain/Tiles/rpoi_shop_01.tile", 5),                size = 1, offset = 0, rotation = 3, flippable = true },
+        { tile = AddTile(4201005, "$CONTENT_DATA/Terrain/Tiles/rpoi_petrol_station_01.tile", 5),      size = 1, offset = 1, rotation = 0, flippable = true },
+        { tile = AddTile(4201006, "$CONTENT_DATA/Terrain/Tiles/rpoi_garage_01.tile", 5),              size = 1, offset = 0, rotation = 1, flippable = true },
+        { tile = AddTile(4201007, "$CONTENT_DATA/Terrain/Tiles/rpoi_observation_tower_01.tile", 5),   size = 1, offset = 2, rotation = 0, flippable = true },
+        { tile = AddTile(4201008, "$CONTENT_DATA/Terrain/Tiles/rpoi_bus_stop_01.tile", 5),            size = 1, offset = 0, rotation = 3, flippable = true },
+        { tile = AddTile(4201009, "$CONTENT_DATA/Terrain/Tiles/rpoi_radio_station_01.tile", 5),       size = 1, offset = 0, rotation = 3, flippable = true },
+        { tile = AddTile(42010010, "$CONTENT_DATA/Terrain/Tiles/rpoi_food_truck_01.tile", 3),         size = 1, offset = 0, rotation = 3, flippable = true },
+        { tile = AddTile(42010011, "$CONTENT_DATA/Terrain/Tiles/rpoi_car_wreck_01.tile", 3),          size = 1, offset = 0, rotation = 3, flippable = true },
+        { tile = AddTile(42010012, "$CONTENT_DATA/Terrain/Tiles/rpoi_truck_wreck_01.tile", 3),        size = 1, offset = 0, rotation = 3, flippable = true },
+    }
 end
 
 ----------------------------------------------------------------------------------------------------
 -- Getters
 ----------------------------------------------------------------------------------------------------
 
-function getDesertTileId( variationNoise )
-	local tileCount = #g_desert
+function getDesertTileId(variationNoise)
+    local tileCount = #g_desert
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_desert[variationNoise % tileCount + 1]
+    return g_desert[variationNoise % tileCount + 1]
 end
 
-function getRoadTileIdAndRotation( variationNoise )
-	local tiles = g_roads.tiles
+function getRoadTileIdAndRotation(variationNoise)
+    local tiles = g_roads.tiles
 
-	local tileCount = #tiles
+    local tileCount = #tiles
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	local rotation = g_roads.rotation
+    local rotation = g_roads.rotation
 
-	return tiles[variationNoise % tileCount + 1], rotation
+    return tiles[variationNoise % tileCount + 1], rotation
 end
 
-function getFenceTileId( variationNoise )
-	local tileCount = #g_fences
+function getFenceTileId(variationNoise)
+    local tileCount = #g_fences
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_fences[variationNoise % tileCount + 1]
+    return g_fences[variationNoise % tileCount + 1]
 end
 
-function getDesertPoi( variationNoise )
-	local tileCount = #g_desert_pois
+function getDesertPoi(variationNoise)
+    local tileCount = #g_desert_pois
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_desert_pois[variationNoise % tileCount + 1]
+    return g_desert_pois[variationNoise % tileCount + 1]
 end
 
-function getRoadPoi( variationNoise )
-	local tileCount = #g_road_pois
+function getRoadPoi(variationNoise)
+    local tileCount = #g_road_pois
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_road_pois[variationNoise % tileCount + 1]
+    return g_road_pois[variationNoise % tileCount + 1]
 end
 
-function getStarterConnector( variationNoise )
-	local tileCount = #g_starter_connectors
+function getStarterConnector(variationNoise)
+    local tileCount = #g_starter_connectors
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_starter_connectors[variationNoise % tileCount + 1]
+    return g_starter_connectors[variationNoise % tileCount + 1]
 end
 
-function getFenceCornerTileId( variationNoise )
-	local tileCount = #g_fence_corners
+function getFenceCornerTileId(variationNoise)
+    local tileCount = #g_fence_corners
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_fence_corners[variationNoise % tileCount + 1]
+    return g_fence_corners[variationNoise % tileCount + 1]
 end
 
-function getScorchedTileId( variationNoise )
-	local tileCount = #g_scorched
+function getScorchedTileId(variationNoise)
+    local tileCount = #g_scorched
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_scorched[variationNoise % tileCount + 1]
+    return g_scorched[variationNoise % tileCount + 1]
 end
 
-function getRoadEndTileId( variationNoise )
-	local tileCount = #g_road_ends
+function getRoadEndTileId(variationNoise)
+    local tileCount = #g_road_ends
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_road_ends[variationNoise % tileCount + 1]
+    return g_road_ends[variationNoise % tileCount + 1]
 end
 
-function getElevatorTileId( variationNoise )
-	local tileCount = #g_elevators
+function getElevatorTileId(variationNoise)
+    local tileCount = #g_elevators
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_elevators[variationNoise % tileCount + 1]
+    return g_elevators[variationNoise % tileCount + 1]
 end
 
-function getHouseTileID( variationNoise )
-	local tileCount = #g_starter_houses
+function getHouseTileID(variationNoise)
+    local tileCount = #g_starter_houses
 
-	if tileCount == 0 then
-		return ERROR_TILE_UUID, 0
-	end
+    if tileCount == 0 then
+        return ERROR_TILE_UUID, 0
+    end
 
-	return g_starter_houses[variationNoise % tileCount + 1]
+    return g_starter_houses[variationNoise % tileCount + 1]
 end
